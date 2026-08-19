a321bf51784254420464b56934693c46bd147e40
vararuunn <varun808088@gmail.com>
2026-08-19 22:57:36 +0530

    Time: 741 ms (5.01%), Space: 9.5 MB (33.07%) - LeetHub

---
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
@@ -0,0 +1,61 @@
+class FizzBuzz {
+private:
+    int n;
+    int cur;
+    mutex mtx;
+
+public:
+    FizzBuzz(int n) {
+        this->n = n;
+        this->cur = 1;
+    }
+
+    // printFizz() outputs "fizz".
+    void fizz(function<void()> printFizz) {
+        while(this->cur <= this->n){
+            if(this->cur % 15 != 0 && this->cur % 3 == 0){
+                mtx.lock();
+                printFizz();
+                this->cur++;
+                mtx.unlock();
+            }
+        }
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function<void()> printBuzz) {
+        while(this->cur <= this->n){
+            if(this->cur % 15 != 0 && this->cur % 5 == 0){
+                mtx.lock();
+                printBuzz();
+                this->cur++;
+                mtx.unlock();
+            }
+        }
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+	void fizzbuzz(function<void()> printFizzBuzz) {
+        while(this->cur <= this->n){
+            if(this->cur % 15 == 0){
+                mtx.lock();
+                printFizzBuzz();
+                this->cur++;
+                mtx.unlock();
+            }
+        }
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void number(function<void(int)> printNumber) {
+        while(this->cur <= this->n){
+            if(this->cur % 3 != 0 && this->cur % 5 != 0){
+                mtx.lock();
+                printNumber(this->cur);
+                this->cur++;
+                mtx.unlock();
+            }
+        }
+    }
+};
+
